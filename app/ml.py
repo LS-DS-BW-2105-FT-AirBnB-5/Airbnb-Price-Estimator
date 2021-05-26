e252477dfbac13c038c3befcc7d15dc02bef070c
yaobviously <yaobviously@gmail.com>
2021-05-26 18:20:12 -0400

    enabled predictions for all three models

--- app/ml.py
+++ app/ml.py
@@ -11,52 +11,68 @@ import pickle
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
+from fastapi.encoders import jsonable_encoder
 
 log = logging.getLogger(__name__)
 router = APIRouter()
-model_location = r'C:\Users\yaobv\group_build\Airbnb-Price-Estimator\models\catboost 75.sav'
-model = pickle.load(open(model_location, 'rb'))
 
-class Item(BaseModel):
+model_location = r'models\catboost 75.sav'
+model_locationb = r'models\catboost_50.sav'
+model_locationc = r'models\catboost_25.sav'
+
+model = pickle.load(open(model_location, 'rb'))
+modelb = pickle.load(open(model_locationb, 'rb'))
+modelc = pickle.load(open(model_locationc, 'rb'))
+
+class RentalUnit(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    borough: str = 'Queens'
+    room_type: str = 'room'
+    accommodates: int = 4
+    day_of_week: int = 6
+    days_until_booking: int = 7
+
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
-        return pd.DataFrame([dict(self)])
+        return pd.DataFrame([self.dict()])
 
-    @validator('x1')
-    def x1_must_be_positive(cls, value):
+    @validator('days_until_booking')
+    def days_until_positive(cls, value):
         """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
+        assert value > 0, f'days until booking == {value}, must be > 0'
         return value
 
 
 @router.post('/predict')
-async def predict(item: Item):
+async def predict():
     """
     Make random baseline predictions for classification problem 🔮
 
     ### Request Body
-    - `x1`: positive float
-    - `x2`: integer
-    - `x3`: string
+    - `borough`: str
+    - `room_type`: str
+    - `accommodates`: int
+    - 'day_of_week' : int
+    - 'days_until_booking' : int
 
     ### Response
-    - `prediction`: boolean, at random
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
+    - `prediction`: estimate of expected price   
 
-    Replace the placeholder docstring and fake predictions with your own model.
+    
     """
-    X_new = item.to_df()
-    log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
+    rentalunit = RentalUnit()
+    rentdict = dict(rentalunit)
+    df = pd.DataFrame(jsonable_encoder(rentalunit), index = [0])
+    # # rentdf = rentalunit.to_df()
+    # rentdf = pd.DataFrame(rentdict, index = [0])
+    # # # log.info(RU_df)
+    y_pred = (model.predict(df)).astype(int)
+    y_predb = (modelb.predict(df)).astype(int)
+    y_predc = (modelc.predict(df)).astype(int)
     return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
+        'prediction high': str(y_pred[0]),
+        'prediction medium': str(y_predb[0]),
+        'prediction low': str(y_predc[0])
     }
